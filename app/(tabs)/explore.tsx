98f7fecabf59a9e86960fccb3e24faf777f7f78b
Tobias <odilsonluz38@gmail.com>
2026-07-30 09:20:31 -0300

    Add files via upload

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function TabTwoScreen() {
+  return (
+    <View>
+      <Text>aaaa (explore)</Text>
+    </View>
+  );
+}
+
+const s = StyleSheet.create({});
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>aaaa</Text>
+    </View>
+  );
+}
+
+const s = StyleSheet.create({});
